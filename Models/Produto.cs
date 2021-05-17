80402cf07094fcd2b9b07b9957824e887d30894e
David San Martín <66731842+sanmartiner@users.noreply.github.com>
2021-05-16 22:59:11 -0400

    Step 1 framework configuring

---
+++ Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao{ get; set; }
+        public int Quantidade{ get; set; }
+        public Categoria categoria { get; set; }
+    }
+}
